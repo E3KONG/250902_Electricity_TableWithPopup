3aa360ac897a8e9467b105eba7b96c5b2c4fcb5b
E3KONG <ethankong.atwork@gmail.com>
2025-09-02 12:46:53 +0800

    feat: setup header & footer & table data-tooltip

---
+++ src/assets/data-store.js
@@ -0,0 +1,19 @@
+﻿import { writable } from 'svelte/store';
+
+export let tableContent = writable([
+	{
+		"核能發展" : "Maria",
+		"Favorite Subject" : "Math",
+		"Age" : 14,
+		"Subject" : "Math",
+		"Tooltip": "This is a tooltip"
+	},
+	{
+		"核能發展" : "Jose",
+		"Favorite Subject" : "Science",
+		"Age" : 13,
+		"Subject" : "Science",
+		"Tooltip": "This is also a tooltip"
+	},
+]);
+
---
+++ src/lib/Table.svelte
@@ -0,0 +1,38 @@
+﻿<script>
+	export let tableData = [
+		{
+			vitae : "dolorem",
+			lectus : "ipsum",
+			quisquam : "quia"
+		},
+		{
+			vitae : "amet",
+			lectus : "consectetur",
+			quisquam : "adipisci"
+		}
+	];
+	export let style;
+
+	const columns = tableData.length
+		? Object.keys(tableData[0]).filter((k) => k !== "Tooltip")
+		: [];
+</script>
+
+<table class={style}>
+	<thead>
+	<tr>
+		{#each columns as columnHeading}
+			<th>{columnHeading}</th>
+		{/each}
+	</tr>
+	</thead>
+	<tbody>
+	{#each tableData as row}
+		<tr>
+			{#each columns as col}
+				<td data-tooltip={row.Tooltip ?? undefined}>{row[col]}</td>
+			{/each}
+		</tr>
+	{/each}
+	</tbody>
+</table>
