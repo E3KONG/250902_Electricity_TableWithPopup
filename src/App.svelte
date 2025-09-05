c2c172656bce13e326fb66e4eae1928dafe0675f
E3KONG <ethankong.atwork@gmail.com>
2025-09-05 17:41:33 +0800

    style: tooltip & party color & rwd

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,12 @@
 	import Table from './lib/Table.svelte';
   import {membersSpeech} from './assets/data-store.js';
 
-	let title = "圖表標題";
+	let title = "2024年第11屆立委上任後，誰討論最多國家能源政策？";
 	let subtitle;
 	let footer = [
-		"註：XXX",
-		"資料來源：",
-		"資料整理：",
-		"設計："
+		"註：截至2025年9月初，立法院第3會期公報未完整公布，本表僅統計2020年至2024年第2會期結束",
+		"資料來源：立法院議事公報、報導者觀測站",
+		"資料整理：簡毅慧  ｜  設計：江世民"
 	];
 </script>
 <div class="container">
